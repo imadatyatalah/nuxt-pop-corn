c29c5b51f52545100d3a82ac004dfb288b8d4243
imad <imadatyatalah@gmail.com>
2021-06-14 20:18:39 +0100

    feat: fetch movie by id

---
+++ pages/movie/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+import { TMDB_BASE_URL, TMDB_BASE_API_KEY } from '@/config'
+
+export default {
+  async asyncData({ params }) {
+    const movieID = await params.id
+
+    const movie = await fetch(
+      `${TMDB_BASE_URL}movie/${movieID}?api_key=${TMDB_BASE_API_KEY}&language=en-US`
+    ).then((res) => res.json())
+
+    return { movie }
+  },
+}
+</script>
+
+<style></style>
