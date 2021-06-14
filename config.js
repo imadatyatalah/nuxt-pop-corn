a1383a13aebe0e9d59f64926ef2e8db34e4292d4
imad <imadatyatalah@gmail.com>
2021-06-14 20:30:17 +0100

    feat: Added config file

---
+++ config.js
@@ -0,0 +1,28 @@
+export const TMDB_BASE_API_KEY = process.env.TMDB_API_KEY
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3/'
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
+
+export const MAX_WIDTH = '1440px'
+
+export const XS_POSTER_SIZE = 'w92'
+export const SM_POSTER_SIZE = 'w154'
+export const MD_POSTER_SIZE = 'w185'
+export const LG_POSTER_SIZE = 'w342'
+export const XL_POSTER_SIZE = 'w500'
+export const XL2_POSTER_SIZE = 'w780'
+export const ORIGINAL_POSTER_SIZE = 'original'
+
+export const XS_PROFILE_SIZE = 'w45'
+export const SM_PROFILE_SIZE = 'w185'
+export const MD_PROFILE_SIZE = 'h632'
+export const ORIGINAL_PROFILE_SIZE = 'original'
+
+export const XS_STILL_SIZE = 'w92'
+export const SM_STILL_SIZE = 'w185'
+export const MD_STILL_SIZE = 'w300'
+export const ORIGINAL_STILL_SIZE = 'original'
+
+export const XS_BACKDROP_SIZE = 'w300'
+export const SM_BACKDROP_SIZE = 'w780'
+export const MD_BACKDROP_SIZE = 'w1280'
+export const ORIGINAL_BACKDROP_SIZE = 'original'
