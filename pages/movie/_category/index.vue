2abc0893d62c7ec544cb53f1659e05d497b915db
imad <imadatyatalah@gmail.com>
2021-06-14 19:52:31 +0100

    feat: fetch movies by category

---
+++ pages/movie/_category/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+import { TMDB_BASE_URL, TMDB_BASE_API_KEY } from '@/config'
+
+export default {
+  async asyncData({ params }) {
+    const movies = await fetch(
+      `${TMDB_BASE_URL}movie/${params.category}?api_key=${TMDB_BASE_API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json())
+
+    return { movies }
+  },
+}
+</script>
+
+<style></style>
