ce1561e4ebe32cb33105558c74417fac18dbad63
imad <imadatyatalah@gmail.com>
2021-06-15 19:21:11 +0100

    feat: Pass movies data to mediaCard component

--- pages/movie/category/_category.vue
+++ pages/movie/category/_category.vue
@@ -1,17 +1,28 @@
 <template>
-  <section></section>
+  <section>
+    <h1>{{ slug }}</h1>
+
+    <div v-for="movie of movies.results" :key="movie.id">
+      <MediaCard :media="movie"></MediaCard>
+    </div>
+  </section>
 </template>
 
 <script>
 import { TMDB_BASE_URL, TMDB_BASE_API_KEY } from '@/config'
+import MediaCard from '@/components/MediaCard.vue'
 
 export default {
+  components: { MediaCard },
+
   async asyncData({ params }) {
+    const slug = await params.category
+
     const movies = await fetch(
-      `${TMDB_BASE_URL}movie/${params.category}?api_key=${TMDB_BASE_API_KEY}&language=en-US&page=1`
+      `${TMDB_BASE_URL}movie/${slug}?api_key=${TMDB_BASE_API_KEY}&language=en-US&page=1`
     ).then((res) => res.json())
 
-    return { movies }
+    return { slug, movies }
   },
 }
 </script>
