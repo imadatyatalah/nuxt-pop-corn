e1a7b57224332c00538bde1257b42fc20770fab2
imad <imadatyatalah@gmail.com>
2021-06-15 19:17:31 +0100

    feat: Added a Tag component

---
+++ components/Tag.vue
@@ -0,0 +1,14 @@
+<template>
+  <span :class="bgColor" class="p-2 mx-1 rounded-[10px] text-sm">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: { bgColor: { type: String, default: 'bg-[#EFE8D8]' } },
+}
+</script>
+
+<style scoped></style>
