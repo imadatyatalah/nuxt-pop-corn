a713532f89b4623ec0cd398ff835712d885d7357
imad <imadatyatalah@gmail.com>
2021-06-15 19:20:01 +0100

    Added mediaPoster component

---
+++ components/MediaPoster.vue
@@ -0,0 +1,28 @@
+<template>
+  <nuxt-img
+    :src="poster"
+    width="342"
+    height="513"
+    :alt="title"
+    :title="title"
+    quality="75"
+  />
+</template>
+
+<script>
+import { poster } from '@/mixins/mediaPoster'
+import { title } from '@/mixins/mediaTitle'
+
+export default {
+  name: 'MediaPoster',
+  mixins: [poster, title],
+
+  props: { media: { type: Object, required: true } },
+}
+</script>
+
+<style scoper>
+img {
+  border-radius: 0.375rem;
+}
+</style>
