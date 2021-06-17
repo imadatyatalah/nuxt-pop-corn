7b55042feb50a82103ed63bf4c70309e314437de
imad <imadatyatalah@gmail.com>
2021-06-17 18:45:46 +0100

    feat: Fetch tvShow data by id and pass it to tvShowDetails comp

--- pages/tv/_id.vue
+++ pages/tv/_id.vue
@@ -1,9 +1,24 @@
 <template>
-  <section></section>
+  <section>
+    <tv-show-details :item="tvShow" />
+  </section>
 </template>
 
 <script>
-export default {}
+import { TMDB_BASE_API_KEY, TMDB_BASE_URL } from '@/config'
+import TvShowDetails from '@/components/TvShowDetails.vue'
+
+export default {
+  components: { TvShowDetails },
+
+  async asyncData({ params }) {
+    const tvShow = await fetch(
+      `${TMDB_BASE_URL}tv/${params.id}?api_key=${TMDB_BASE_API_KEY}&language=en-US`
+    ).then((res) => res.json())
+
+    return { tvShow }
+  },
+}
 </script>
 
-<style></style>
+<style scoped></style>
