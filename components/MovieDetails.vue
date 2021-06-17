8d863ba0b4980629fcaee676fc334974c3520087
imad <imadatyatalah@gmail.com>
2021-06-17 18:43:52 +0100

    feat: Added movie/tvShow details page components

---
+++ components/MovieDetails.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <div class="page-container">
+      <div class="flex justify-center">
+        <media-poster :media="item" />
+      </div>
+
+      <div>
+        <div class="text-center lg:text-left py-4 lg:pt-0">
+          <h1 class="title">{{ item.title }}</h1>
+
+          <div class="pt-4">
+            <p>
+              <tag>{{ item.release_date | date }}</tag>
+              <tag>{{ genres }}.</tag>
+              <tag v-if="item.runtime !== 0">{{ runtime }}</tag>
+            </p>
+
+            <h2 v-if="item.tagline" class="tagline">
+              {{ item.tagline }}
+            </h2>
+          </div>
+        </div>
+
+        <ul>
+          <li>
+            <strong class="label">Overview: </strong>
+            <p class="value">{{ item.overview }}</p>
+          </li>
+
+          <li v-if="directors">
+            <strong class="label">Director: </strong>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <p class="value" v-html="directors" />
+          </li>
+
+          <li>
+            <strong class="label">Status: </strong>
+            <p class="value">{{ item.status }}.</p>
+          </li>
+
+          <li v-if="item.budget">
+            <strong class="label">Budget: </strong>
+            <p class="value">${{ item.budget | numberToPrice }}.</p>
+          </li>
+
+          <li v-if="item.revenue">
+            <strong class="label">Revenue: </strong>
+            <p class="value">${{ item.revenue | numberToPrice }}.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { genres } from '@/mixins/mediaGenres'
+import { runtime } from '@/mixins/runtime'
+import { directors } from '@/mixins/mediaDirector'
+import MediaPoster from '@/components/MediaPoster.vue'
+import Tag from '@/components/Tag.vue'
+
+export default {
+  name: 'MovieDetails',
+  components: { MediaPoster, Tag },
+  mixins: [genres, runtime, directors],
+
+  props: {
+    item: { type: Object, required: true },
+    itemCredits: { type: Object, required: true },
+  },
+}
+</script>
+
+<style scoped>
+@import url('~/assets/css/detailsPage.css');
+</style>
---
+++ components/TvShowDetails.vue
@@ -0,0 +1,94 @@
+<template>
+  <section>
+    <div class="page-container">
+      <div class="flex justify-center">
+        <media-poster :media="item" />
+      </div>
+
+      <div>
+        <div class="text-center lg:text-left py-4 lg:pt-0">
+          <h1 class="title">{{ item.name }}</h1>
+
+          <div class="pt-4">
+            <p>
+              <tag>
+                {{ item.number_of_seasons }}
+                {{ item.number_of_seasons === 1 ? 'Season' : 'Seasons' }}
+              </tag>
+
+              <tag>
+                {{ item.number_of_episodes }}
+                {{ item.number_of_episodes === 1 ? 'Episode' : 'Episodes' }}
+              </tag>
+
+              <tag>{{ genres }}.</tag>
+              <tag>{{ episodesRuntime }}</tag>
+            </p>
+
+            <h2 v-if="item.tagline" class="tagline">
+              {{ item.tagline }}
+            </h2>
+          </div>
+        </div>
+
+        <ul>
+          <li>
+            <strong class="label">Overview: </strong>
+            <p class="value">{{ item.overview }}</p>
+          </li>
+
+          <li v-if="creators">
+            <strong class="label">Creator: </strong>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <p class="value" v-html="creators" />
+          </li>
+
+          <li>
+            <strong class="label">Status: </strong>
+            <p class="value">{{ item.status }}.</p>
+          </li>
+
+          <li v-if="item.budget">
+            <strong class="label">Budget: </strong>
+            <p class="value">${{ item.budget | numberToPrice }}.</p>
+          </li>
+
+          <li v-if="item.revenue">
+            <strong class="label">Revenue: </strong>
+            <p class="value">${{ item.revenue | numberToPrice }}.</p>
+          </li>
+
+          <li>
+            <strong class="label">First Aired: </strong>
+            <p class="value">{{ item.first_air_date | date }}</p>
+          </li>
+
+          <li>
+            <strong class="label">Last Aired: </strong>
+            <p class="value">{{ item.last_air_date | date }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { genres } from '@/mixins/mediaGenres'
+import { episodesRuntime } from '@/mixins/runtime'
+import { creators } from '@/mixins/mediaCreator'
+import MediaPoster from '@/components/MediaPoster.vue'
+import Tag from '@/components/Tag.vue'
+
+export default {
+  name: 'MovieDetails',
+  components: { MediaPoster, Tag },
+  mixins: [genres, episodesRuntime, creators],
+
+  props: { item: { type: Object, required: true } },
+}
+</script>
+
+<style scoped>
+@import url('~/assets/css/detailsPage.css');
+</style>
