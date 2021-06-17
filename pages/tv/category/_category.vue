b7a65a6f76d67de6799cdf6fd1fb3e4347db6e87
imad <imadatyatalah@gmail.com>
2021-06-17 18:46:27 +0100

    feat: Fetch tvShows by category and pass it to MediaCards comp

--- pages/tv/category/_category.vue
+++ pages/tv/category/_category.vue
@@ -1,9 +1,24 @@
 <template>
-  <section></section>
+  <section>
+    <media-cards :items="tvShows" media-type="tv" />
+  </section>
 </template>
 
 <script>
-export default {}
+import { TMDB_BASE_URL, TMDB_BASE_API_KEY } from '@/config'
+import MediaCards from '@/components/MediaCards.vue'
+
+export default {
+  components: { MediaCards },
+
+  async asyncData({ params }) {
+    const tvShows = await fetch(
+      `${TMDB_BASE_URL}tv/${params.category}?api_key=${TMDB_BASE_API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json())
+
+    return { tvShows }
+  },
+}
 </script>
 
-<style></style>
+<style scoped></style>
