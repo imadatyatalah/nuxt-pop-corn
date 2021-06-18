d45eed5afedf483c657c31a5ecbaec2c601be713
imad <imadatyatalah@gmail.com>
2021-06-18 10:59:02 +0100

    feat: Added some global styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
+  <main>
     <Nuxt />
-  </div>
+  </main>
 </template>
 
-<style></style>
+<style>
+main {
+  @apply tracking-wide px-4 sm:px-8 md:px-12 lg:px-16 max-w-[1440px] mx-auto;
+}
+</style>
