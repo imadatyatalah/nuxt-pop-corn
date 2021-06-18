d5384f79a3b34f1431c46fb08c41a43b23ac9741
imad <imadatyatalah@gmail.com>
2021-06-18 11:00:04 +0100

    feat: Use MovieDetails comp

--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -1,48 +1,28 @@
 <template>
-  <section class="grid grid-cols-[auto,1fr] gap-x-[165px] mx-[165px]">
-    <MediaPoster :media="movie" />
-
-    <div class="">
-      <h1 class="text-6xl font-bold">{{ movie.title }}</h1>
-
-      <p class="py-6">
-        <Tag>{{ movie.release_date }}</Tag>
-        <Tag>{{ genres }}.</Tag>
-        <Tag v-if="movie.runtime !== 0">{{ runtime }}</Tag>
-      </p>
-
-      <div v-if="movie.overview">
-        <h2 class="font-semibold text-lg">overview</h2>
-
-        <p class="pt-[6px]">
-          {{ movie.overview }}
-        </p>
-      </div>
-    </div>
+  <section>
+    <movie-details :item="movie" :item-credits="movieCredits" />
   </section>
 </template>
 
 <script>
 import { TMDB_BASE_URL, TMDB_BASE_API_KEY } from '@/config'
-import { genres } from '@/mixins/mediaGenres'
-import { runtime } from '@/mixins/runtime'
-import MediaPoster from '@/components/MediaPoster.vue'
-import Tag from '@/components/Tag.vue'
+import MovieDetails from '@/components/MovieDetails.vue'
 
 export default {
-  components: { MediaPoster, Tag },
-  mixins: [genres, runtime],
+  components: { MovieDetails },
 
   async asyncData({ params }) {
-    const movieID = await params.id
-
     const movie = await fetch(
-      `${TMDB_BASE_URL}movie/${movieID}?api_key=${TMDB_BASE_API_KEY}&language=en-US`
+      `${TMDB_BASE_URL}movie/${params.id}?api_key=${TMDB_BASE_API_KEY}&language=en-US`
     ).then((res) => res.json())
 
-    return { movie }
+    const movieCredits = await fetch(
+      `${TMDB_BASE_URL}movie/${params.id}/credits?api_key=${TMDB_BASE_API_KEY}&language=en-US`
+    ).then((res) => res.json())
+
+    return { movie, movieCredits }
   },
 }
 </script>
 
-<style></style>
+<style scoped></style>
